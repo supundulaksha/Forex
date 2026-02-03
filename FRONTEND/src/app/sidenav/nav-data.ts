40e268e945d9fead4d1492d086992f3e46de3079
Supun Dulaksha Niluminda <127416465+supundulaksha@users.noreply.github.com>
2026-02-03 22:36:19 +0530

    Update: Dashboard UI Changes

--- FRONTEND/src/app/sidenav/nav-data.ts
+++ FRONTEND/src/app/sidenav/nav-data.ts
@@ -1,52 +1,66 @@
 import { INavbarData } from './helper';
+
 export const navbarData: INavbarData[] = [
   {
-    routerLink: '',
-    icon: 'fa-sharp fa-solid fa-house',
+    routerLink: 'dashboard',
+    icon: 'fas fa-tachometer-alt',
     label: 'Dashboard',
-    items: [
-      {
-        routerLink: 'dashboard',
-        label: 'Dashboard',
-      },
-    ],
   },
   {
     routerLink: '',
-    icon: 'fa-solid fa-database',
-    label: ' Master',
+    icon: 'fas fa-database',
+    label: 'Master',
     items: [
       {
         routerLink: 'title',
         label: 'Title',
-      },{
+      },
+      {
         routerLink: 'menu-types',
         label: 'Menu Types',
-      },{
+      },
+      {
         routerLink: 'reservation-types',
         label: 'Reservation Types',
-      },{
+      },
+      {
         routerLink: 'tables',
         label: 'Tables',
-      },{
+      },
+      {
         routerLink: 'menu-items',
         label: 'Menu Items',
-      },{
-        routerLink: 'chef-master',
-        label: 'chef Items',
       },
-
+      {
+        routerLink: 'chef-master',
+        label: 'Chef Master',
+      },
     ],
   },
   {
     routerLink: '',
-    icon: 'fa-solid fa-database',
-    label: ' Customer',
+    icon: 'fas fa-users',
+    label: 'Customer',
     items: [
       {
         routerLink: 'customer-form',
         label: 'Customer Form',
       }
-      ],
+    ],
+  },
+  {
+    routerLink: '',
+    icon: 'fas fa-utensils',
+    label: 'Kitchen',
+    items: [
+      {
+        routerLink: 'kitchen-dashboard',
+        label: 'Kitchen Dashboard',
+      },
+      {
+        routerLink: 'orders',
+        label: 'Orders',
+      }
+    ],
   }
 ];
